775684d4bdaa50d55ea2c1ed75d558d9872b575c
Quintin Strickland <147731117+stricklandq20@users.noreply.github.com>
2023-11-28 10:14:19 -0500

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -170,7 +170,7 @@ const config = {
 
   // Optional Footer. Supports plain text or HTML.
   footer: `Made with <a 
-      class="text-primary" href="https://github.com/arifszn/gitprofile"
+      class="text-primary" href="https://github.com/stricklandq20/gitprofile"
       target="_blank"
       rel="noreferrer"
     >GitProfile</a> and ❤️`,
